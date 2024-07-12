9b62928ab5db9579a2b10c9d05bd6b6e25b13dba
Ardi Germenji <ardi.germenji@gmail.com>
2024-07-12 18:05:03 +0100

    refactor: Improve EmojiSticker component with animated scaling and double tap gesture

--- components/emoji-sticker.js
+++ components/emoji-sticker.js
@@ -1,14 +1,39 @@
 import React from 'react'
-import { Image, View } from 'react-native'
+import { View } from 'react-native'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated'
 
 export const EmojiSticker = ({ stickerSource, imageSize }) => {
+  const scaleImage = useSharedValue(imageSize)
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    }
+  })
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2
+      }
+    })
+
   return (
     <View style={{ top: -350 }}>
-      <Image
-        source={stickerSource}
-        resizeMode='contain'
-        style={{ width: imageSize, height: imageSize }}
-      />
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image
+          source={stickerSource}
+          resizeMode='contain'
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </GestureDetector>
     </View>
   )
 }
